59975fbb73c72e28a5e1d27da5ed7f2e8dd55c5b
Anupam Kushwaha <anupamkushwaha1426@gmail.com>
2021-07-08 05:58:47 +0530

    fixed bug in js

--- tha/day10/script.js
+++ tha/day10/script.js
@@ -15,5 +15,49 @@ for (let i = 1; i <= 16; i++) {
     let front = document.createElement("div");
     front.classList.add("front");
 
-    let back = document.createElement()
+    let back = document.createElement("div");
+    let rand = Math.floor(Math.random() * (16 - i));
+
+    back.innerHTML = "<h2>" + words[rand] + "</h2>";
+    words.splice(rand, 1);
+    back.classList.add("back");
+
+    inner.appendChild(front);
+    inner.appendChild(back);
+
+    card.appendChild(inner);
+    document.getElementById("grid").appendChild(card);
+}
+
+let clicked;
+let cards = document.getElementsByClassName("flip-card");
+
+for (let card of cards) {
+    card.addEventListener("click", (e) => {
+        if (!gameActive) return;
+        moves--;
+        document.querySelector("span").innerHTML = moves;
+        if (clicked) {
+            card.firstChild.classList.add("flipped");
+            gameActive = false;
+            setTimeout(() => {
+                if (
+                    card.firstChild.lastChild.innerHTML !==
+                    clicked.firstChild.lastChild.innerHTML
+                ) {
+                    card.firstChild.classList.remove("flipped");
+                    clicked.firstChild.classList.remove("flipped");
+                } else {
+                    matches++;
+                    card.style.visibility = "hidden";
+                    clicked.style.visibility = "hidden";
+                }
+                clicked = undefined;
+                gameActive = true;
+            }, 500);
+        } else {
+            clicked = card;
+            card.firstChild.classList.add("flipped");
+        }
+    });
 }
