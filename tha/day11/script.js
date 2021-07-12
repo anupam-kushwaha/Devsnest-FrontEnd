0d2ba7e828a70e18f92740e10ed2c54c61dfcafa
Anupam Kushwaha <anupamkushwaha1426@gmail.com>
2021-07-12 10:23:15 +0530

    updated day11

---
+++ tha/day11/script.js
@@ -0,0 +1,25 @@
+const questions = [
+    {
+        question: "Who is the owner of bitcoin?",
+        option: ["Satoshi Nakamoto", "Elon Musk", "Jeff Bezos", "Bill Gates"],
+    }
+]
+
+questions.forEach((current, number)=> {
+    console.log(current, number)
+    const container = document.createElement()
+})
+
+
+
+/**
+ * <div class="container">
+                <div class="question">1. Who is owner of bitcoin?</div>
+                <div class="options">
+                    <div class="option" ><button class="correct">option1</button></div>
+                    <div class="option"><button class="wrong">option2</button></div>
+                    <div class="option"><button>option3</button></div>
+                    <div class="option"><button>option4</button></div>
+                </div>
+            </div>
+*/
